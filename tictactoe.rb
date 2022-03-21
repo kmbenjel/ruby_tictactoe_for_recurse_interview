f981978ec33212da70c80f9b6afd39191ad178ff
kmbenjel <khalidmailing@gmail.com>
2022-03-21 10:04:30 +0100

    Recent edit

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,13 +1,15 @@
 require './design.rb'
 puts "------------------"
+sleep(0.2)
 puts "   Tic-Tac-Toe   "
+sleep(0.2)
 puts "------------------"
 sleep(1)
 puts "\n"
 puts "****  WELCOME ABROAD  ****"
 puts "\n"
-sleep(2)
-print "Player one: what's your name?: "
+sleep(1.5)
+print "Player one: What's your name?: "
 player_one = gets.chomp
 puts "Welcome, #{player_one}!"
 print "Player two: what's your name?: "
