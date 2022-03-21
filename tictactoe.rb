aaacaa40f83b0d6e228753fa8b87398a73826ac7
kmbenjel <khalidmailing@gmail.com>
2022-03-21 10:05:53 +0100

    Recent edit

--- tictactoe.rb
+++ tictactoe.rb
@@ -11,9 +11,10 @@ puts "\n"
 sleep(1.5)
 print "Player one: What's your name?: "
 player_one = gets.chomp
-puts "Welcome, #{player_one}!"
+puts "Nice to meet you, #{player_one}!"
 print "Player two: what's your name?: "
 player_two = gets.chomp
+puts "Nice to meet you, #{player_one}!"
 
 puts "Let's go!"
      
